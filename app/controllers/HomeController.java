716c1184026a93201cdb374a6c3b45a13f9e5e5d
wilson <wilson@emobi.biz>
2018-05-30 01:18:27 +0700

    add query string

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package controllers;
 
-import play.mvc.*;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+import play.mvc.Controller;
+import play.mvc.Result;
 
-import views.html.*;
-
+import java.util.Map;
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
@@ -17,6 +19,10 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+        Map<String, String[]> queryString = request().queryString();
+        if (CollectionUtils.isEmpty(queryString)) {
+            return badRequest();
+        }
         return ok("Your new application is ready.");
     }
 
