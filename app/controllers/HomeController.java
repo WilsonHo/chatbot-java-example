bcd9983f94946d54a36229de1bbfcbd5e3cab6b6
wilson <wilson@emobi.biz>
2018-05-30 01:22:05 +0700

    back to old project

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -6,6 +6,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.Map;
+
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
@@ -19,11 +20,33 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+        return ok("Your new application is ready.");
+    }
+
+    private static final String accessToken = "EAAGAZCKE1pF4BAM3Jalc0ZAPT6jZBDgGkC0LoIXwHiVFU9pf75dhiQPPdPDLySJ79EXDZC5HJE55irKN7FvHiLikNnLEoZBllMQ7FO6K3kSo3lZCv8fsnlSSmfbaxjZCygsPUPjAue2ZCJ9OzKYGriDwV23JgP8ZBYflnXMch04w8fgZDZD";
+    private static final String verifyToken = "chatbot-java-example";
+
+    public Result chatbotJavaExample() {
         Map<String, String[]> queryString = request().queryString();
         if (CollectionUtils.isEmpty(queryString)) {
             return badRequest();
         }
-        return ok("Your new application is ready.");
+
+        String hubToken = StringUtils.EMPTY;
+        String hubChallenge = StringUtils.EMPTY;
+        if (queryString.get("hub.verify_token") != null && queryString.get("hub.verify_token").length > 0) {
+            hubToken = queryString.get("hub.verify_token")[0];
+        }
+
+        if (queryString.get("hub.challenge") != null && queryString.get("hub.challenge").length > 0) {
+            hubChallenge = queryString.get("hub.challenge")[0];
+        }
+
+        if (verifyToken.equals(hubToken)) {
+            return ok(hubChallenge);
+        } else {
+            return ok("Hello Wrong");
+        }
     }
 
 }
